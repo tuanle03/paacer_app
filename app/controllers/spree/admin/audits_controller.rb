5dcb9f52d2bff762230ca925e890a7e4d6c8373b
Tuan Le <letuanvl03@gmail.com>
2025-04-29 05:29:50 +0700

    feat(admin): add filtering to audits index page

--- app/controllers/spree/admin/audits_controller.rb
+++ app/controllers/spree/admin/audits_controller.rb
@@ -2,7 +2,16 @@ module Spree
   module Admin
     class AuditsController < Spree::Admin::BaseController
       def index
-        @audits = Audited::Audit.order(created_at: :desc).page(params[:page]).per(25)
+        audits_scope = Audited::Audit.order(created_at: :desc)
+
+        if params[:q].present?
+          audits_scope = audits_scope.where(
+            auditable_id: params[:q][:auditable_id_eq],
+            auditable_type: params[:q][:auditable_type_eq]
+          )
+        end
+
+        @audits = audits_scope.page(params[:page]).per(21)
       end
     end
   end
